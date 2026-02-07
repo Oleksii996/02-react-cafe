9c522e914d32edb7727db7f54817abc47a9e7c66
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-02-07 18:22:17 +0100

    update code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import type { Votes } from "../../types/votes";
+
+type VoteType = keyof Votes;
 
 const App = () => {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  // Функція для обробки голосування
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  // Функція для скидання всіх голосівä l,
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const canReset = votes.good > 0 || votes.neutral > 0 || votes.bad > 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={canReset} />
+      <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
     </div>
   );
 };
