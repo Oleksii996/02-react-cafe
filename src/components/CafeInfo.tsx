0b82fac56fee66c43ee915570f742ad0f9ba1783
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-02-07 14:48:17 +0100

    update code

---
+++ src/components/CafeInfo.tsx
@@ -0,0 +1,4 @@
+<div className={css.container}>
+  <h1 className={css.title}>Sip Happens Café</h1>
+  <p className={css.description}>Please rate our service by selecting one of the options below.</p>
+</div>;
